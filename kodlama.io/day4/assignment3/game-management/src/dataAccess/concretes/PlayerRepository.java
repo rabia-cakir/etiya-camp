8382780f44906fae84835c8fd271b8170537ca4b
rabia.cakir <rabiacakir0681@gmail.com>
2022-07-23 22:49:24 +0300

    revised v2

--- kodlama.io/day4/assignment3/game-management/src/dataAccess/concretes/PlayerRepository.java
+++ kodlama.io/day4/assignment3/game-management/src/dataAccess/concretes/PlayerRepository.java
@@ -3,26 +3,32 @@ package dataAccess.concretes;
 import dataAccess.abstracts.DataRepository;
 import entities.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlayerRepository implements DataRepository<Player> {
+    List<Player> players=new ArrayList<>();
     @Override
     public List<Player> getAll() {
-        return null;
+        return players;
     }
 
     @Override
     public void add(Player entity) {
+        players.add(entity);
         System.out.println("player added");
     }
 
     @Override
     public void update(Player entity) {
+        System.out.println("player updated");
 
     }
 
     @Override
     public void delete(Player entity) {
+        players.remove(entity);
+        System.out.println("player deleted");
 
     }
 }
