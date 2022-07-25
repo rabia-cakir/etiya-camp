29c468805f629889e76497cebd8c07f6e98e08ee
rabia.cakir <rabiacakir0681@gmail.com>
2022-07-25 15:57:31 +0300

    revised v4

---
+++ kodlama.io/DbWorkshop/src/IndividualCustomer.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class IndividualCustomer extends Customer{
+    private String firstName;
+    private String lastName;
+    private String identityNumber;
+
+    public IndividualCustomer() {
+    }
+
+    public IndividualCustomer(int id, String customerNumber, Canal canal,
+                              String firstName, String lastName, String identityNumber) {
+        super(id, customerNumber, canal);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.identityNumber=identityNumber;
+
+    }
+}
